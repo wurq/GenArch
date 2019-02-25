8466ffa3cbcd72a4ff512f0cdf1cc573a473923c
wurongqiu <wurongqiu@kaike.la>
2019-02-25 22:03:39 +0800

    add RN activity

--- app/src/main/java/com/arch/genarch/application/GenArchApplication.java
+++ app/src/main/java/com/arch/genarch/application/GenArchApplication.java
@@ -3,11 +3,20 @@ package com.arch.genarch.application;
 import android.app.Application;
 import android.content.Context;
 
+import com.arch.genarch.BuildConfig;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by wurongqiu on 2017/4/26.
  */
 
-public class GenArchApplication extends Application {
+public class GenArchApplication extends Application  implements ReactApplication {
     @Override
     public void onCreate() {
         super.onCreate();
@@ -25,5 +34,21 @@ public class GenArchApplication extends Application {
     @Override
     public void onLowMemory() {
         super.onLowMemory();
+    }
+
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+        @Override
+        protected List getPackages() {
+            return Arrays.<ReactPackage>asList(   new MainReactPackage());
+        }
+    };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 }
--- app/src/main/java/com/arch/genarch/module/rnpage/activity/RNActivity.java
+++ app/src/main/java/com/arch/genarch/module/rnpage/activity/RNActivity.java
@@ -2,35 +2,40 @@ package com.arch.genarch.module.rnpage.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.support.annotation.Nullable;
 
-import com.arch.genarch.R;
+import com.facebook.react.ReactActivity;
 
-public class RNActivity extends AppCompatActivity {
+public class RNActivity extends ReactActivity {
 
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_rn);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//    }
+
+    private static final String MAIN_COMPONENT = "ReactNativeDemo";
+
+    /**     * 返回在index.android.js 中注册的组件名
+     * @return
+     */    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rn);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    protected String getMainComponentName() {
+         return MAIN_COMPONENT;
+     }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    public static void start(Context context) {
+     public static void start(Context context) {
         Intent starter = new Intent(context, RNActivity.class);
 //        starter.putExtra();
         context.startActivity(starter);
